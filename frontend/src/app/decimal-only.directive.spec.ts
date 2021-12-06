2a79781ad06309b14a13d4699b87166190bc6232
Mikolaj Skrzyniarz <mikolaj.skrzyniarz@grandparade.co.uk>
2021-12-06 20:58:40 +0100

    Fixed startup Karma bugs.

--- frontend/src/app/decimal-only.directive.spec.ts
+++ frontend/src/app/decimal-only.directive.spec.ts
@@ -1,8 +1,13 @@
 import { DecimalOnlyDirective } from './decimal-only.directive';
+import { ElementRef } from '@angular/core';
+
+class MockElementRef implements ElementRef {
+  nativeElement = {};
+}
 
 describe('DecimalOnlyDirective', () => {
   it('should create an instance', () => {
-    const directive = new DecimalOnlyDirective();
+    const directive = new DecimalOnlyDirective(new MockElementRef());
     expect(directive).toBeTruthy();
   });
 });
