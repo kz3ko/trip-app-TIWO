1b72d5541d344008ddd524ed26c76fae963748af
kz3ko <xiixikx@gmail.com>
2022-01-23 18:20:02 +0100

    One more AppRootComponent test.

--- frontend/src/app/app-root/app-root.component.spec.ts
+++ frontend/src/app/app-root/app-root.component.spec.ts
@@ -2,15 +2,26 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AppRootComponent } from './app-root.component';
+import {AuthService} from "../auth-service.service";
+import {of} from "rxjs";
+
+class AuthServiceStub {
+  loggedIn = false;
+  accessLevel = ['regular'];
+  loggedInStream$ = of(this.loggedIn);
+  accessLevelsStream$ = of(this.accessLevel);
+}
 
 describe('AppRootComponent', () => {
   let component: AppRootComponent;
   let fixture: ComponentFixture<AppRootComponent>;
+  let authServiceStub: Partial<AuthService>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ AppRootComponent ],
       imports: [ HttpClientTestingModule ],
+      providers: [ { provide: AuthService, useValue: new AuthServiceStub() }]
     })
     .compileComponents();
   });
@@ -18,10 +29,17 @@ describe('AppRootComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(AppRootComponent);
     component = fixture.componentInstance;
+    authServiceStub = TestBed.inject(AuthService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  // INTEGRATION TESTS
+  it('should use service when assigning properties', () => {
+    expect(component.loggedIn).toBe(authServiceStub.loggedIn);
+    expect(component.isAdmin).toBeFalsy();
   });
 });
